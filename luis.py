0c8ee75db2e828a1a3bac09112ef753adc0fb7eb
Marco Adrián Guido <mguido@ndscognitivelabs.com>
2019-12-30 18:53:54 -0600

    todo: create dataset before uploading data implementation

---
+++ luis.py
@@ -0,0 +1,165 @@
+"""
+    implements the interface for setting and getting data from LUIS
+
+    todos:
+        create dataset and test it
+        create unittests
+"""
+import os
+import yaml
+import json
+import time
+import datetime
+from pprint import pprint
+from azure.cognitiveservices.language.luis.authoring import LUISAuthoringClient
+from msrest.authentication import CognitiveServicesCredentials
+
+del luis
+class luis:
+    """
+    https://github.com/microsoft/Cognitive-LUIS-Python
+    https://github.com/Azure-Samples/cognitive-services-python-sdk-samples/blob/9ebf063909771ec6d03cba42ccec9eecdec6e538/samples/language/luis/luis_authoring_samples.py
+    """
+    def __init__(self):
+        #TODO: try with yaml app id, else: fall down to get onlien workspaces
+        self.creds = self.getCreds()
+        self.assistant = LUISAuthoringClient(
+            'https://westus.api.cognitive.microsoft.com',
+            CognitiveServicesCredentials(self.creds["luis"]["APIKEY"]),
+        )
+        self.app_id = self.getWorkspaceID()
+        self.version_id = "0.1"
+
+    def getCreds(self, credsPath = None):
+        if credsPath == None:
+            credsPath = os.path.join("creds","creds.yaml")
+        creds = {}
+        with open( credsPath, "r" )  as stream:
+            try:
+                creds = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        return creds
+
+    def getWorkspaceID(self):
+        def getApp():
+            return self.assistant.apps.list()[0].id
+            apps = getApps()
+        self.app_id = getApp()
+        return self.app_id
+
+    def getIntents(self):
+        intents = self.assistant.model.list_intents(self.app_id,self.version_id)
+        return [intent.id for intent in intents if intent.name != 'None']
+
+    def deleteIntent(self,intent):
+        self.assistant.model.delete_intent(
+            self.app_id,
+            self.version_id,
+            intent,
+            delete_utterances = True
+            )
+
+    def deleteAllIntents(self):
+        for intent in self.getIntents():
+            self.deleteIntent(intent)
+
+
+    """
+    try:
+        # Create a LUIS app
+        default_app_name = "Contoso-{}".format(datetime.datetime.now())
+        version_id = "0.1"
+
+        app_id = a.assistant.apps.add({
+            'name': default_app_name,
+            'initial_version_id': version_id,
+            'description': "New App created with LUIS Python sample",
+            'culture': 'en-us',
+        })
+
+        destination_name = "Destination"
+        destination_id = a.assistant.model.add_entity(
+            app_id,
+            version_id,
+            name = "testName",
+            raw= True
+        )
+
+        class_name = "Class"
+
+
+        flight_name = "Flight"
+
+        find_economy_to_madrid = "find flights in economy to Madrid"
+        find_first_to_london = "find flights to London in first class"
+
+        intent_name = "FindFlights"
+        intent_id = a.assistant.model.add_intent(
+            app_id,
+            version_id,
+            intent_name
+        )
+
+        utterance = {
+            'text': find_economy_to_madrid,
+            'intent_name': intent_name,
+            }
+        a.assistant.examples.add(
+            app_id,
+            version_id,
+            utterance,
+            raw = True
+        )
+
+
+        utterances = [
+            {
+                'text': find_economy_to_madrid,
+                'intent_name': intent_name,
+            },
+            {
+                'text': find_first_to_london,
+                'intent_name': intent_name,
+            }
+        ]
+
+
+        utterances_result = a.assistant.examples.batch(
+            app_id,
+            version_id,
+            utterances,
+            raw = True
+        )
+
+        async_training = a.assistant.train.train_version(app_id, version_id, raw = True)
+        async_training = a.assistant.train.train_version(app_id, version_id, raw = False)
+
+        is_trained = async_training.status == "UpToDate"
+
+        trained_status = ["UpToDate", "Success"]
+        while not is_trained:
+            time.sleep(1)
+            status = a.assistant.train.get_status(app_id, version_id)
+            is_trained = all(
+                m.details.status in trained_status for m in status)
+
+        publish_result = a.assistant.apps.publish(
+            app_id,
+            version_id = version_id,
+            is_staging = False,
+            region = 'westus'
+        )
+
+        endpoint = publish_result.endpoint_url + \
+            "?subscription-key=" + a.creds["luis"]["APIKEY"] + "&q="
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+    """
+
+a.getWorkspaceID()
+a.getIntents()
+a.deleteAllIntents()
+
+a = luis()
--- watson.py
+++ watson.py
@@ -1,8 +1,95 @@
-import yaml
+"""
+    implements the interface for setting and getting data from watson assistant
 
-creds = {}
-with open( "creds.yaml" , "r" )  as stream:
-    try:
-        print(yaml.safe_load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
+    todos:
+        create dataset and test it
+        create unittests
+"""
+
+import os
+import yaml
+from ibm_watson import AssistantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import requests
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+del watson
+
+class watson:
+    """
+    https://cloud.ibm.com/apidocs/assistant/assistant-icp?code=python#create-intent
+    """
+    def __init__(self):
+
+        self.creds = self.getCreds()
+
+        self.authenticator = IAMAuthenticator(self.creds["watson"]["APIKEY"])
+        self.assistant = AssistantV1(
+            version='2018-07-10',
+            authenticator=self.authenticator)
+        self.assistant.set_service_url(self.creds["watson"]["URL"])
+
+        self.assistant.set_disable_ssl_verification(True)
+
+    def getCreds(self, credsPath= None):
+        if credsPath == None:
+            credsPath = os.path.join("creds","creds.yaml")
+        creds = {}
+        with open( credsPath, "r" )  as stream:
+            try:
+                creds = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        return creds
+
+    def getWorkspaceID(self):
+        return self.assistant.list_workspaces().get_result()["workspaces"][0]["workspace_id"]
+
+
+    def getResponse(self, msg = ""):
+        response = self.assistant.message(
+            workspace_id=self.creds["watson"]["WORKSPACE_ID"],
+            input={
+                'text': 'bye'
+            }
+        ).get_result()
+        return response
+
+    def setData(self, dataPath = "data.yaml"):
+
+        raise Exception("not implemented yet")
+
+        data = {}
+        with open( dataPath , "r" )  as stream:
+            try:
+                data = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        for d in data:
+            response= self.assistant.create_intent(
+                workspace_id=self.creds["watson"]["WORKSPACE_ID"],
+                intent=d.intent,
+                examples=d.listofUtterances
+            ).get_result()
+
+    def getIntents(self):
+        #TODO: add pagination
+        return self.assistant.list_intents(
+                workspace_id=self.creds["watson"]["WORKSPACE_ID"]
+                ).get_result()["intents"]
+
+    def deleteIntent(self, intent):
+        returnresponse= self.assistant.delete_intent(
+            workspace_id=self.creds["watson"]["WORKSPACE_ID"],
+            intent=intent["intent"]
+        ).get_result()
+
+    def deleteAllIntents(self):
+        for intent in self.getIntents()["intents"]:
+            self.deleteIntent(intent)
+
+a = watson()
+a.getWorkspaceID()
+a.getIntents()
