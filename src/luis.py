0b7775c905ec6ce8ef9bddb347b37b6e804ae2e2
Marco Adrián Guido <mguido@ndscognitivelabs.com>
2020-01-02 20:03:27 -0600

    finishing up luis and 3 new datasets added

--- src/luis.py
+++ src/luis.py
@@ -14,7 +14,7 @@ from pprint import pprint
 from azure.cognitiveservices.language.luis.authoring import LUISAuthoringClient
 from msrest.authentication import CognitiveServicesCredentials
 from baseAssistant import baseAssistant
-
+import pandas as pd
 
 class luis(baseAssistant):
     """
@@ -69,90 +69,48 @@ class luis(baseAssistant):
     def getResponse(self, msg = ""):
         raise Exception("not implemented yet!")
 
-    def setData(self, data):
-        #change this for group in pandas and yaml for csv
-        for key in data.counter.keys:
-            for i, label in enumerate(labels):
-                if label== key:
+    def setData(self, filename = "data.csv"):
+        def setEntityData(intent, utterancesList):
+            intent_id = self.assistant.model.add_intent(
+                self.app_id,
+                self.version_id,
+                intent
+            )
 
-        class_name = "Class"
-        flight_name = "Flight"
+            utterances = []
+            for utterance in utterancesList:
+                utterances.append({'text': utterance, 'intent_name':intent})
 
-        find_economy_to_madrid = "find flights in economy to Madrid"
-        find_first_to_london = "find flights to London in first class"
+            #TODO: simplify
+            for i in range(1 + len(utterances)//100):
+                utterances_result = self.assistant.examples.batch(
+                    self.app_id,
+                    self.version_id,
+                    utterances[i*100:(i+1)*100]
+                )
 
-        intent_name = "FindFlights"
-        intent_id = a.assistant.model.add_intent(
-            app_id,
-            version_id,
-            intent_name
-        )
-
-        utterance = {
-            'text': find_economy_to_madrid,
-            'intent_name': intent_name,
-            }
-        a.assistant.examples.add(
-            app_id,
-            version_id,
-            utterance,
-            raw = True
-        )
-
-
-        utterances = [
-            {
-                'text': find_economy_to_madrid,
-                'intent_name': intent_name,
-            },
-            {
-                'text': find_first_to_london,
-                'intent_name': intent_name,
-            }
-        ]
-
-
-        utterances_result = a.assistant.examples.batch(
-            self.app_id,
-            self.version_id,
-            utterances,
-            raw = True
-        )
-
+        df = pd.read_csv(os.path.join("..","data",filename))
+        for label in df["labels"].unique():
+            utterancesList = df["sentences"].loc[df["labels"] == label].values
+            setEntityData(label, utterancesList)
 
     def update(self):
-        async_training = a.assistant.train.train_version(self.app_id, self.version_id, raw = True)
-
-        async_training = a.assistant.train.train_version(self.app_id, self.version_id, raw = False)
-
+        #async_training = a.assistant.train.train_version(self.app_id, self.version_id, raw = True)
+        async_training = self.assistant.train.train_version(self.app_id, self.version_id, raw = False)
         is_trained = async_training.status == "UpToDate"
-
         trained_status = ["UpToDate", "Success"]
         while not is_trained:
             time.sleep(1)
-            status = a.assistant.train.get_status(app_id, version_id)
+            status = self.assistant.train.get_status(self.app_id, self.version_id)
             is_trained = all(
                 m.details.status in trained_status for m in status)
 
-        publish_result = a.assistant.apps.publish(
-            app_id,
-            version_id = version_id,
+        publish_result = self.assistant.apps.publish(
+            self.app_id,
+            self.version_id,
             is_staging = False,
             region = 'westus'
         )
 
         self.endpoint = publish_result.endpoint_url + \
             "?subscription-key=" + a.creds["luis"]["APIKEY"] + "&q="
-
-
-
-if __name__() == "__main__":
-    a = luis()
-    a.update()
-    a.getWorkspaceID()
-    a.getIntents()
-    a.deleteAllIntents()
-    async_training = a.assistant.train.train_version(a.app_id, a.version_id, raw = True)
-    async_training.response.status
-    dir(async_training)
-    dir(async_training.response)
